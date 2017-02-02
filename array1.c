6fceed85daac1f994310a5d19ac9f99817471465
Santhakumar96 <santhakumar.eee96@gmail.com>
2017-02-02 19:42:53 +0530

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,20 @@
+#include<studio.h>
+
+int main(void)
+{
+        int a[10],i,s;
+        for(i=0;i<5;i++)
+         {
+                  scanf("%d",&a[i]);
+        }
+        scanf("%d",&n);
+
+       for(i=0;i<5;i++)
+       {
+               if(a[i]==s)
+               {
+                        printf("%d",i);
+               }
+        }
+      return 0;
+}
